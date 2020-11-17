2e02e28346535cad1e9c505c2a419d29f159c697
Abigail Schofield <aschofield24@biomedscienceacademy.org>
2020-11-17 18:51:31 +0000

    finished the project

--- main.py
+++ main.py
@@ -2,54 +2,53 @@
 MadLibs
 Author: Abby Schofield
 Period/Core:  7
-
-
+ 
+ 
 """
-
+ 
 print("Enter a noun")
 madlib1 = str(input())
-
+ 
 print("Enter an adjective")
 madlib2 = str(input())
-
+ 
 print("Enter a plural noun")
 madlib3 = str(input())
-
+ 
 print("Enter a diffrent noun")
 madlib4 = str(input())
-
+ 
 print("Enter a diffrent plural noun")
 madlib5 = str(input())
-
+ 
 print("Enter a diffrent adjective")
 madlib6 = str(input())
-
+ 
 print("Enter a diffrent noun")
 madlib7 = str(input())
-
+ 
 print("Enter a verb ending with ing")
 madlib8 = str(input())
-
+ 
 print("Enter a part of the body")
 madlib9 = str(input())
-
+ 
 print("Enter a part of the body plural")
 madlib10 = str(input())
-
+ 
 print("Enter another part of the body plural")
 madlib11 = str(input())
-
+ 
 print("Enter an adverb ending with ly")
 madlib12 = str(input())
-
+ 
 print("Enter a color")
-madlib13 = str((input()
-
+madlib13 = str(input())
+ 
 print("Enter a part of the body")
 madlib14 = str(input())
-
+ 
 print("Enter a diffrent part of the body")
 madlib15 = str(input())
-
-
-print ('Surprisingly a skunk is a friendly',madlib1,'who can make a',madlib2,'house hold pet.\nBut what makes these colorful',madlib3,'smell to high',madlib4,'? \nThe skunk has a scent',madlib5,'that contain a',madlib6,'smelling fluid.\nWhen attacked that skunk aims this smelly',madlib7,'at its enemies.\nBut the skunk does give a warning before',madlib8,'.\nIt raises its',madlib9,'first or stamps its',madlib10,'so that you can run way as fast as your',madlib11,'can carry you.\nThe most',madlib12,'reconginzable skunk is the one with a',madlib13,'line on its',madlib14,'and another one between its',madlib15,' and its ears.')
+ 
+print (f"Surprisingly a skunk is a friendly {madlib1} who can make a {madlib2} house hold pet.\nBut what makes these colorful{madlib3}smell to high {madlib4}? \nThe skunk has a scent {madlib5} that contain a {madlib6} smelling fluid.\nWhen attacked that skunk aims this smelly {madlib7} at its enemies.\nBut the skunk does give a warning before{madlib8}.\nIt raises its {madlib9}first or stamps its{madlib10}so that you can run way as fast as your {madlib11} can carry you.\nThe most'{madlib12}'reconginzable skunk is the one with a{madlib13}'line on its'{madlib14}and another one between its{madlib15}and its ears.")
